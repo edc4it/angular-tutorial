d6c94eb77da1f333f90b14cea8c7aef597950021
Raphael Parree <rparree@edc4it.com>
2021-10-11 16:53:30 +0200

    feat: upgraded to webpack 5.x and ng 12.x

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import {NgModule} from "@angular/core";
-import {FormsModule} from "@angular/forms";
 import {BrowserModule} from "@angular/platform-browser";
 import {HelloComponent} from "./app.component";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
-    imports:      [BrowserModule, FormsModule ],
-    bootstrap:    [HelloComponent],
-    declarations: [HelloComponent],
+    imports:      [BrowserModule, FormsModule ], // import Angular's BrowserModule
+    bootstrap:    [HelloComponent],  // indicate the bootstrap component
+    declarations: [HelloComponent] // register our component with the module
 })
 export class AppModule {}
--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,60 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require("webpack");
-const FilterWarningsPlugin = require('webpack-filter-warnings-plugin');
 
 module.exports = {
-    mode: 'development',
+    entry: {
+        polyfills: "./src/polyfills.ts", // add this
+        main: "./src/app.ts" //change this
+    },
     resolve: {
         extensions: ['.js', '.ts']
     },
-    entry: {
-        polyfills: "./src/polyfills.ts",
-        main: "./src/app.ts"
+    devtool: "source-map",
+    devServer: {
+        historyApiFallback: true
     },
     output: {
         path: path.resolve(__dirname, 'dist'), // output directory
         filename: "[name].js" // name of the generated bundle
     },
     module: {
-        rules: [{
-            test: /\.css$/,
-            loader: ["style-loader", "css-loader"]
-        },
+        rules: [
             {
                 test: /\.ts$/,
-                loader: "awesome-typescript-loader"
-            },
-            {
-                test: /\.ts$/,
-                enforce: "pre",
-                loader: 'tslint-loader'
+                loader: "ts-loader",
+                exclude: /node_modules/,
             },
             {
                 test: /\.html$/,
-                loader: "html-loader"
-            },
-            {
-                test: /\.scss$/,
-                loader: ["raw-loader", "sass-loader?sourceMap"]
-            }
-        ]
-    },
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                    chunks: 'initial',
-                    name: 'vendor',
-                    test: 'vendor',
+                loader: "html-loader",
+                options: {
+                    esModule: false,
                 },
             },
-        }
+{
+    test: /\.scss$/,
+    use: [
+        {
+            loader: "raw-loader",
+            options: {
+                esModule: false,
+            }
+        },
+        {
+            loader: "sass-loader",
+            options: {
+                sourceMap: true,
+            },
+        },
+    ],
+},
+        ]
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: "src/index.html",
-            inject: "body"
-        }),
-
-        new webpack.ContextReplacementPlugin(
-            /\@angular(\\|\/)core(\\|\/)fesm5/,
-            path.resolve(__dirname, 'src'),{}
-        ),
-        new FilterWarningsPlugin({
-            exclude: /System.import/
+            template: "./src/index.html",
+            inject: "body",
+            scriptLoading: "blocking"
         })
-    ],
-    devtool: "source-map",
-    devServer: {
-        historyApiFallback: true
-    }
+    ]
 };
