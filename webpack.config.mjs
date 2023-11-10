b79f4ca4274fe20f0ac5a5de49cfce495e383f94
Thomas Lhomme <lhomme.thomas@gmail.com>
2023-11-10 17:15:07 +0100

    Moving to V16

---
+++ webpack.config.mjs
@@ -0,0 +1,85 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { AngularWebpackPlugin } from "@ngtools/webpack";
+import linkerPlugin from '@angular/compiler-cli/linker/babel';
+
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+    entry: {
+        polyfills: "./src/polyfills.ts", // add this
+        main: "./src/app.ts" //change this
+    },
+    resolve: {
+        extensions: ['.js', '.ts']
+    },
+    devtool: "source-map",
+    devServer: {
+        historyApiFallback: true
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'), // output directory
+        filename: "[name].js" // name of the generated bundle
+    },
+    module: {
+        rules: [
+            {
+                test: /\.[jt]sx?$/,
+                loader: "@ngtools/webpack",
+            },
+            {
+                test: /\.html$/,
+                loader: "html-loader",
+                options: {
+                    esModule: false,
+                },
+            },
+            {
+                test: /\.m?js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        plugins: [linkerPlugin],
+                        compact: false,
+                        cacheDirectory: true,
+                    }
+                }
+            },
+            {
+                test: /\.ts$/,
+                loader: "ts-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: "raw-loader",
+                        options: {
+                            esModule: false,
+                        }
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                ],
+            }
+        ]
+    },
+    plugins: [
+        new AngularWebpackPlugin({
+            tsconfig: "./tsconfig.json",
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            inject: "body",
+            scriptLoading: "blocking"
+        })
+    ]
+};
